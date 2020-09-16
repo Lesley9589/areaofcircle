000cc2a04951135d35a163dab672ff4eb8678c56
lesley9589 <lesleylamont9589@gmail.com>
2020-09-16 16:36:34 +0200

    my initial commit

---
+++ Hello.py
@@ -0,0 +1,14 @@
+print("Hello Guys! How are you?")
+print("Learning Python is Fun with Life Choices")
+print(5+9)
+'''
+firstname=input("What is your name?")
+print("My name is  ", firstname)
+'''
+
+#=============================================
+radius=int(input("please enter radius"))
+
+pi=3.14
+area=pi * radius*radius
+print(round(area, 2))
